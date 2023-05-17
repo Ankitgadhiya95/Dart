757e0072a34d477937d37cd77f95d79343da5107
Ankitgadhiya95 <117982509+Ankitgadhiya95@users.noreply.github.com>
2023-05-17 14:51:14 +0530

    Create 4_1.dart

---
+++ 4_1.dart
@@ -0,0 +1,17 @@
+Task__1-->
+
+void main() {
+  List a = List.generate(
+    3,
+    (index) {
+      return index+1;
+    },
+  );
+var i=1;
+  List b=[];
+  for(int i=0;i<3;i++)
+  {
+    b.add("${a[i]} Hello ");
+  }
+print(b);
+}
