2624ccb0837b4e1ecdf189a396f86f7d562e63a0
Ankitgadhiya95 <117982509+Ankitgadhiya95@users.noreply.github.com>
2023-05-18 17:06:29 +0530

    Update 6_1.dart

--- 6_1.dart
+++ 6_1.dart
@@ -35,3 +35,60 @@ void main() {
     ),
   ));
 }
+
+
+
+Task__2--->
+  
+  import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: Colors.red,
+          title: Text("Mission of RNW"),
+        ),
+        backgroundColor: Colors.white,
+        body: Center(
+          child: Container(
+            padding: EdgeInsets.zero,
+            margin: EdgeInsets.zero,
+            width: 300,
+            height: 150,
+            decoration: BoxDecoration(
+              color: Color(0XFFFCC8C8),
+              border: Border(
+                left: BorderSide(
+                  width: 8,
+                  color: Color(0XFFFF5252),
+                ),
+              ),
+            ),
+            child: Text.rich(
+              TextSpan(
+                children: [
+                  TextSpan(
+                    text: "\n-RNW",
+                    style:
+                        TextStyle(fontWeight: FontWeight.normal, fontSize: 20),
+                  )
+                ],
+                text: "Shaping'skills'for'scaling'higher",
+                style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black),
+              ),
+            ),
+            alignment: Alignment.center,
+          ),
+        ),
+      ),
+    ),
+  ));
+}
