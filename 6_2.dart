fcacb1d5d9467e3244539a7713d1aa2c07bf2025
Ankitgadhiya95 <117982509+Ankitgadhiya95@users.noreply.github.com>
2023-05-24 15:26:25 +0530

    Create 6_2.dart

---
+++ 6_2.dart
@@ -0,0 +1,46 @@
+Task__1--->
+  
+  
+  import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            centerTitle: true,
+            backgroundColor: Color(0XFF4CAF50),
+            title: Text(
+              "Launch Button",
+              style: TextStyle(fontSize: 25),
+            ),
+          ),
+          backgroundColor: Colors.black,
+          body: Center(
+            child: Container(
+              child: Center(
+                child: Text(
+                  "Go",
+                  style: TextStyle(color: Colors.white, fontSize: 50),
+                ),
+              ),
+              width: 220,
+              height: 220,
+              decoration: BoxDecoration(
+                  boxShadow: [
+                    BoxShadow(color: Colors.green, blurRadius: 80),
+                  ],
+                  shape: BoxShape.circle,
+                  color: Color(0XFF000000),
+                  border: Border.all(color: Colors.white, width: 1)),
+            ),
+          ),
+        ),
+      ),
+    ),
+  );
+}
