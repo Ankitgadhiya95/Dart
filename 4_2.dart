e7fc4a411a9f1f41c46670a642b335d1c1f3f192
Ankitgadhiya95 <117982509+Ankitgadhiya95@users.noreply.github.com>
2023-06-15 13:47:10 +0530

    Create 4_2.dart

---
+++ 4_2.dart
@@ -0,0 +1,40 @@
+Task--1
+  
+
+import 'dart:io';
+
+void main() {
+  List newlist = [];
+  for (var i = 0; i <= 2; i++) {
+    print("Enter the Id");
+    String id = stdin.readLineSync()!;
+    print("Enter the Name");
+    String name = stdin.readLineSync()!;
+    print("Enter the Age");
+    String age = stdin.readLineSync()!;
+    print("Enter the salary");
+    String salary = stdin.readLineSync()!;
+
+    MapEntry<String, String> entry1 = MapEntry('id', '$id');
+
+    MapEntry<String, String> entry2 = MapEntry('name', '$name');
+
+    MapEntry<String, String> entry3 = MapEntry('age', '$age');
+    MapEntry<String, String> entry4 = MapEntry('salary', '$salary');
+
+    Map map = Map.fromEntries([entry1, entry2, entry3, entry4]);
+
+    newlist.add(map);
+  }
+
+  var newmap = newlist.asMap();
+  for (int i = 0; i < newmap.length; i++) {
+    print(" ID :${newmap[i]["id"]}");
+
+    print(" NAME:${newmap[i]["name"]}");
+
+    print("AGE:${newmap[i]["age"]}");
+
+    print("SALARY ${newmap[i]["salary"]}");
+  }
+}
