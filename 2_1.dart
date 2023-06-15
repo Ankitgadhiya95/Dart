22f9fe0dae308ac80648db3a169b76ca396827c3
Ankitgadhiya95 <117982509+Ankitgadhiya95@users.noreply.github.com>
2023-06-15 13:30:45 +0530

    Update 2_1.dart

--- 2_1.dart
+++ 2_1.dart
@@ -16,3 +16,50 @@ void main() {
   print(a + b);
 }
 
+
+Task--3
+  
+  import 'dart:io';
+
+void main() {
+  int a, b;
+  print("Enter num1");
+  a = int.parse(stdin.readLineSync()!);
+  print("Enter num2");
+  b = int.parse(stdin.readLineSync()!);
+
+  print("The $a * $b = ${a * b}");
+}
+
+Task--4
+  
+  import 'dart:io';
+
+void main() {
+  int a, b;
+  print("Enter Number of table");
+  int number = int.parse(stdin.readLineSync()!);
+  for (int i = 1; i <= 10; i++) {
+    var output = number * i;
+    print("$number * $i = ${number * i}");
+  }
+}
+
+Task--5
+  
+  import 'dart:io';
+
+void main() {
+  print("Enter value Of Table Number:");
+  int start = int.parse(stdin.readLineSync()!);
+  print("Enter value Of Table Number:");
+  int end = int.parse(stdin.readLineSync()!);
+  for (int j = start; j <= end; j++) {
+    for (int i = 1; i <= 10; i++) {
+      var output = start * i;
+      print("$start * $i = $output");
+    }
+    print("\n");
+    start++;
+  }
+}
