b4bb2ce2d2d08190782925a43e944297f78d2275
Ankitgadhiya95 <117982509+Ankitgadhiya95@users.noreply.github.com>
2023-06-17 17:39:30 +0530

    Update 7_1.dart

--- 7_1.dart
+++ 7_1.dart
@@ -1,1 +1,173 @@
+Task--1
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(
+        title: 'Calc',
+      ),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+            // backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+            backgroundColor: Color(0XFF54759E),
+            title: Text(
+              'Calc',
+              style: TextStyle(color: Colors.white),
+            ),
+            leading: Icon(
+              Icons.menu,
+              color: Colors.white,
+            ),
+            centerTitle: true,
+          ),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              // crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  '$_counter',
+                  style: TextStyle(
+                    fontSize: 100,color: Color(0XFF9E9E9E)
+                  ),
+                ),
+                Column(
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          height: 50,
+                          width: 120,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: Color(0XFF54759E),
+                          ),
+                          child: Center(
+                              child: Icon(
+                            Icons.exposure_minus_2_rounded,size: 35,color: Colors.white,
+                          )),
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Container(
+                          height: 50,
+                          width: 120,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: Color(0XFF54759E),
+                          ),
+                          child: Center(
+                              child: Icon(
+                            Icons.exposure_minus_2_rounded,
+                            size: 35,color: Colors.white,
+                          )),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: Color(0XFF54759E),
+                          ),
+                          height: 50,
+                          width: 120,
+                          child: Center(
+                              child: Icon(
+                            Icons.exposure_minus_2_rounded,size: 35,color: Colors.white,
+                          )),
+                        ),
+                        SizedBox(
+                          width: 20,
+                        ),
+                        Container(
+                          height: 50,
+                          width: 120,
+                          decoration: BoxDecoration(
+                            borderRadius: BorderRadius.circular(5),
+                            color: Color(0XFF54759E),
+                          ),
+                          child: Center(
+                              child: Icon(
+                            Icons.exposure_minus_2_rounded,
+                            size: 35,color: Colors.white,
+                          )),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Column(
+                  children: [
+                    Container(
+                      height: 50,
+                      width: 120,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        color: Color(0XFF54759E),
+                      ),
+                      child: Center(child: Text("Clear",style: TextStyle(fontSize: 30,color: Colors.white,),)),
+                    )
+                  ],
+                )
+              ],
+            ),
+          )
+          // floatingActionButton: FloatingActionButton(
+          //   onPressed: _incrementCounter,
+          //   tooltip: 'Increment',
+          //   child: const Icon(Icons.add),
+          ), // This trailing comma makes auto-formatting nicer for build methods.
+      // ),
+    );
+  }
+}
 
